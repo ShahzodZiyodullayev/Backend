79f540b3620fdbe9f23354c8441103548353f70d
ShahzodZiyodullayev <sshahzod5@gmail.com>
2020-12-11 20:33:24 +0500

    Tanlash operatoriga oid masalarning 6 gachasi

--- index4.js
+++ index4.js
@@ -102,4 +102,56 @@ switch(kunlarSoni){
    case 9:
    case 11:
       console.log("4. Bu oyda 30 kun mavjud");
+      break;
+   default:
+      console.log("4. Xatolik");
+      break;
 }
+
+// 5. Berilgan sonlar ustida arifmetik amal bajaruvchi dastur tuzilsin
+
+let A = 10;
+let B = 20;
+let amal = Math.floor(Math.random() * 4 + 1);
+switch(amal){
+   case 1:
+      console.log("5. Sonlar qo'shildi: " + (A + B));
+      break;
+   case 2:
+      console.log("5. Sonlar ayirildi: " + (B - A));
+      break;
+   case 3:
+      console.log("5. Sonlar bo'lindi: " + (B / A));
+      break;
+   case 4:
+      console.log("5. Sonlar ko'paytirildi: " + (A * B));
+      break;
+   default:
+      console.log("5. Bunday qiymat mavjud emas");
+      break;
+}
+
+// 6. Kesma uzunligini uzunlik birliklarida ifodalang
+
+let kesmaUzunligi_sm = Math.floor(Math.random() * 100 + 1);
+let birliklar = Math.floor(Math.random() * 5 + 1);
+switch(birliklar){
+   case 1:
+      console.log("6. Kesma uzunligi: " + (kesmaUzunligi_sm / 10) + " detsimetr");
+      break;
+   case 2:
+      console.log("6. Kesma uzunligi: " + (kesmaUzunligi_sm / 100000) + " kilometr");
+      break;
+   case 3:
+      console.log("6. Kesma uzunligi: " + (kesmaUzunligi_sm / 100) + " metr");;
+      break;
+   case 4: 
+      console.log("6. Kesma uzunligi: " + (kesmaUzunligi_sm / 0.1) + " millimetr");
+      break;
+   case 5:
+      console.log("6. Kesma uzunligi: " + (kesmaUzunligi_sm / 1) + " santimetr");
+      break;
+   default:
+      console.log("6. Xatolik");
+      break;
+} 
