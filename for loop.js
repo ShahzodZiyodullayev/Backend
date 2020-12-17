9c8b3cc14809bf2f4aaf930b0c534cf54968f364
ShahzodZiyodullayev <sshahzod5@gmail.com>
2020-12-17 12:26:18 +0500

    Bir nechta fayllar nomlari almashtirildi va yangilari qo'shildi

---
+++ for loop.js
@@ -0,0 +1,177 @@
+// 1
+
+// let k = 2;
+// let n = 5;
+// if(n > 0){
+//    for(let i = 0; i < n; i++){
+//       console.log(k)
+//    }
+// }
+
+// 2
+
+// let a = 10;
+// let b = 20;
+// if(a < b){
+//    for(let i = a; i <= b; i++){
+//       console.log(i);
+//    }
+// }
+// console.log("Chiqarilgan sonlar soni: " ,b - a + 1);
+
+// 3
+
+// let a = 10;
+// let b = 12;
+// if(a < b){
+//    for(let i = b; i >= a; i--){
+//       console.log(i)
+//    }
+// }
+// console.log("Chiqarilgan sonlar soni: " ,b - a + 1);
+
+// 4
+
+// let a = 5; 
+// for(let i = 1; i <= 10; i++){
+//    console.log(i, "*", a, "=", i * a);
+// }
+
+// 5
+
+// let a = 5;
+// for(let i = 1; i <= 10; i++){
+//    console.log(i / 10, "*", a, "=", (i / 10) * a);
+// }
+
+// 6
+
+// let a = 5;
+// for(let i = 1.2; i <= 2; i = i + 0.2){
+//    console.log(i, "*", a, "=", i * a);
+// }
+
+// 7
+
+// let a = 10;
+// let b = 30;
+// let sum = 0;
+// for(let i = a; i <= b; i++){
+//    console.log(sum += i);
+// }
+
+// 8
+
+// let a = 10;
+// let b = 30;
+// let sum = 1;
+// for(let i = a; i <= b; i++){
+//    console.log(sum *= i);
+// }
+
+// 9
+
+// let a = 1;
+// let b = 4;
+// let sum = 0;
+// for(let i = a; i <= b; i++){
+//    console.log(sum += Math.pow(i, 2));
+// }
+
+// 10
+
+// let n = 3;
+// let sum = 0;
+// for(let i = 1; i <= n; i++){
+//    console.log(sum += 1 / i);
+// }
+
+// 11
+
+// let n = 3;
+// let sum = 0;
+// for(let i = 0; i <= 5; i++){
+//    console.log(sum += Math.pow((n + i), 2));
+// }
+
+// 12
+
+// let n = 4;
+// let sum = 1;
+// for(let i = 1.1; i <= 4; i = i + 0.1){
+//    console.log(sum *= i);
+// }
+
+// 13
+
+// let n = 1.3;
+// let sum = 0;
+// for(let i = 1.1; i <= n; i += 0.2){
+//    console.log("1",sum += i);
+// }
+// for(let j = 1.2; j <= n; j = j + 0.2){
+//    console.log("2",sum -= j);
+// }
+
+// 14
+
+// let sum = 0;
+// let n = 7;
+// for(let i = 1; i <= n; i++){
+//    console.log(sum += (2 * i - 1));
+// }
+
+// 15
+
+// let n = 4;
+// let a = 2;
+// let sum = 1;
+// for(let i = 1; i <= n; i++){
+//    console.log(sum *= a);
+// }
+
+// 16
+
+// let n = 4;
+// let a = 3;
+// let sum = 1;
+// for(let i = 1; i <= n; i++){
+//    console.log(sum *= a);
+// }
+
+// 17
+
+// let n = 4;
+// let a = 3;
+// let sum = 0;
+// for(let i = 0; i <= n; i++){
+//    console.log(sum += Math.pow(a,i));
+// }
+
+// 18
+
+// let n = 4;
+// let a = 3;
+// let sum = 0;
+// for(let i = 0; i <= n; i++){
+//    console.log(sum += Math.pow((-1),i) * Math.pow(a,i));
+// }
+
+// 19
+
+// let n = 4;
+// let sum = 1;
+// for(let i = 1; i <= n; i++){
+//    console.log(sum *= i);
+// }
+
+// 20
+
+// let n = 4;
+// let sum = 1;
+// for(let i = 1; i <= n; i++){
+//    console.log(sum += );
+// }
+
+// 21
+
