92f134e7b1c26c3e2dd97954db58ed4db97a6d0e
ShahzodZiyodullayev <sshahzod5@gmail.com>
2020-12-25 00:14:07 +0500

    Yangi fayllar qo'shildi

--- string.js
+++ string.js
@@ -1,21 +1,21 @@
 // Berilgan satrda elementlar sonini aniqlovchi dastur tuzing
 
-// let str = "Anglay olsang, har nafasing baxt...";
-// let obj = new Object();
-// obj.string = "";
-// for(let i = 0; i < str.length; i++){
-//    if(str[i] !== " "){
-//       obj.string += str[i];
-//    }
-// }
-// console.log(obj.string.length)
+let str1 = "Anglay olsang, har nafasing baxt...";
+let obj = new Object();
+obj.string = "";
+for(let i = 0; i < str1.length; i++){
+   if(str1[i] !== " "){
+      obj.string += str1[i];
+   }
+}
+console.log(obj.string.length)
 
 
 // “toshkent“ so`zidagi asosiy harfni bosh harfda chiqaruvchi funksiya tuzing
 
-// let toshkent = "toshkent";
-// let Toshkent = toshkent[0].toUpperCase();
-// console.log(Toshkent + toshkent.slice(1))
+let toshkent = "toshkent";
+let Toshkent = toshkent[0].toUpperCase();
+console.log(Toshkent + toshkent.slice(1))
 
 // Ism, familiyani kichik harflarda kiritganda uni bosh harflarda yozuvchi dastur tuzing
 
@@ -26,16 +26,14 @@ function upperCase(name1){
    let lastName = name.slice(name.indexOf(' ') + 1, name.length);
    let one = firstName[0].toUpperCase() + firstName.slice(1);
    let two = lastName[0].toUpperCase() + lastName.slice(1);
-   // console.log(lastName)
    return console.log(new Object({
       result: one + " " + two
    }))
-
 }
 upperCase(str)
 // Berilgan vaqtdan keladigan sanagacha qolgan kunlarni aniqlovchi dastur tuzing
 
-let next = new Date(2022, 0, 1);
+let next = new Date(2021, 0, 1);
 let previous = new Date();
 let qoldi = next - previous;
 let kun = Math.floor(qoldi / 1000 / 60 / 60 / 24);
